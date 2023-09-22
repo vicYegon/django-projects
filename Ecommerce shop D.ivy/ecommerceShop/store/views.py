939aed3c87dccb0222b4b6fb984ca730080a2550
vicYegon <vkosgei37@gmail.com>
2023-09-23 00:30:20 +0300

    add-to-cart

--- Ecommerce shop D.ivy/ecommerceShop/store/views.py
+++ Ecommerce shop D.ivy/ecommerceShop/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 # Create your views here.
@@ -14,7 +15,8 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items =order.orderitem_set.all()
     else:
-        item = []
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
 
     context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
@@ -26,7 +28,12 @@ def checkout(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items =order.orderitem_set.all()
     else:
-        item = []
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
 
     context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
