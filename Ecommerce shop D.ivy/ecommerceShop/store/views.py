80fcc6c308df6bc794f2304fa9f64e0d157d9f2c
vicYegon <vkosgei37@gmail.com>
2023-09-22 15:54:00 +0300

    views

--- Ecommerce shop D.ivy/ecommerceShop/store/views.py
+++ Ecommerce shop D.ivy/ecommerceShop/store/views.py
@@ -16,9 +16,17 @@ def cart(request):
     else:
         item = []
 
-    context = {'items':items}
+    context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items =order.orderitem_set.all()
+    else:
+        item = []
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
