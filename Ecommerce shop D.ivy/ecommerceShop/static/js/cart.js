acb9c0774e45d111d3a65e85832e4d9441aad4e3
vicYegon <vkosgei37@gmail.com>
2023-09-30 12:00:24 +0300

    add to cart fix

--- Ecommerce shop D.ivy/ecommerceShop/static/js/cart.js
+++ Ecommerce shop D.ivy/ecommerceShop/static/js/cart.js
@@ -1,13 +1,13 @@
-var updateBtns = document.getElementsByClassName('update-cart');
+let updateBtn = document.getElementsByClassName('update-cart');
 
 
-for(var i = 0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function(){
+for(let i = 0; i < updateBtn.length; i++){
+    updateBtn[i].addEventListener('click', function(){
         var productId = this.dataset.product;
         var action = this.dataset.action;
-        console.log('productId:', productId, 'Action:', action);
+        console.log('productId:', productId, 'action:', action);
     
-        console.log('USER:', user)
+        console.log('USER:', user);
         if(user === 'AnonymousUser'){
             console.log('User is not authenticated');
         }
@@ -18,9 +18,9 @@ for(var i = 0; i < updateBtns.length; i++){
 }
 
 function updateUserOrder(productId, action){
-    console.log('user is logged in');
+    console.log("User logged in, sending data...")
 
-    var url = '/update_item/';
+    var url = 'update_item/';
 
     fetch(url, {
         method: 'POST', 
@@ -34,8 +34,7 @@ function updateUserOrder(productId, action){
     .then((response) => {
         return response.json();
     })
-
     .then((data) => {
-        console.log('data:', data);
+        console.log ('data:', data);
     })
 }
--- Ecommerce shop D.ivy/ecommerceShop/store/views.py
+++ Ecommerce shop D.ivy/ecommerceShop/store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
+import json
 
 # Create your views here.
 def store(request):
@@ -36,4 +37,30 @@ def checkout(request):
 
 
 def updateItem(request):
-    return JsonResponse('Item was added', safe=False)
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        productId = data['productId']
+        action = data['action']
+
+        print('action:', action)
+        print('productId', productId)
+
+        customer = request.user.customer
+        product = Product.objects.get(id=productId)
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+        orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+        if action == 'add':
+            orderItem.quantity = (orderItem.quantity + 1)
+        elif action == 'remove':
+            orderItem.quantity = (orderItem.quantity - 1)
+
+        orderItem.save()
+
+        if orderItem.quantity <= 0:
+            orderItem.delete()
+
+        return JsonResponse({'message':'Item added'}, safe=False)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
